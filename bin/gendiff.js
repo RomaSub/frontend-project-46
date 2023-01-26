04dbbd64d903d0214141e7fbe2eaf6acd381da5d
Roma1371 <antonfrolov1212@gmail.com>
2023-01-26 09:17:13 +0300

    commit

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,7 @@ const program = new Command();
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.8.0', '-v, --version', 'output the version number')
+.arguments('<filepath1> <filepath2>')
+.option('-f, --format <type>', 'output format')
 
 program.parse();
