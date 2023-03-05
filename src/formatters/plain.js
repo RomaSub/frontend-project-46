056dd0eb435ee006105b736c57d0863b3f107b54
Roma1371 <antonfrolov1212@gmail.com>
2023-03-05 20:40:04 +0300

    fix3

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,38 +1,37 @@
 import _ from 'lodash';
 
-const buildString = (data) => {
+const stringify = (data) => {
   if (_.isObject(data)) {
     return '[complex value]';
   }
-  return typeof data === 'string' ? `'${data}'` : String(data);
+
+  if (typeof data === 'string') {
+    return `'${data}'`;
+  }
+
+  return String(data);
 };
 
-const plain = (data) => {
-  const iter = (current, path) => {
-    const result = current
-      .filter((diff) => diff.type !== 'unchanged')
-      .flatMap((diff) => {
-        const { key } = diff;
-        const newPath = [...path, key];
+const iter = (nodes, path) => nodes.flatMap((node) => {
+  const { key } = node;
+  const newPath = [...path, key];
 
-        switch (diff.type) {
-          case 'added':
-            return `Property '${newPath.join('.')}' was added with value: ${buildString(diff.value)}`;
-          case 'deleted':
-            return `Property '${newPath.join('.')}' was removed`;
-          case 'changed':
-            return `Property '${newPath.join('.')}' was updated. From ${buildString(diff.value1)} to ${buildString(diff.value2)}`;
-          case 'nested':
-            return iter(diff.children, newPath);
-          default:
-            throw new Error(`Unknown type: ${diff.type}`);
-        }
-      });
+  switch (node.type) {
+    case 'added':
+      return `Property '${newPath.join('.')}' was added with value: ${stringify(node.value)}`;
+    case 'deleted':
+      return `Property '${newPath.join('.')}' was removed`;
+    case 'changed':
+      return `Property '${newPath.join('.')}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+    case 'unchanged':
+      return [];
+    case 'nested':
+      return iter(node.children, newPath);
+    default:
+      throw new Error(`Unknown type: ${node.type}`);
+  }
+});
 
-    return `${result.join('\n')}`;
-  };
+const formatPlain = (tree) => `${iter(tree, []).join('\n')}`;
 
-  return iter(data, []);
-};
-
-export default plain;
+export default formatPlain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,43 +1,41 @@
 import _ from 'lodash';
 
-const getStringIndent = (depth, spacesCount = 4) => ' '.repeat((depth * spacesCount) - 2);
-const getBracketIndent = (depth, spacesCount = 4) => ' '.repeat((depth * spacesCount) - spacesCount);
+const getIndent = (depth, spacesCount = 4) => ' '.repeat(depth * spacesCount - 2);
 
-const buildString = (data, depth) => {
+const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
 
   const lines = Object
     .entries(data)
-    .map(([key, value]) => `${getStringIndent(depth + 1)}  ${key}: ${buildString(value, depth + 1)}`);
+    .map(([key, value]) => `${getIndent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
 
-  return ['{', ...lines, `${getBracketIndent(depth + 1)}}`].join('\n');
+  return `{\n${lines.join('\n')}\n  ${getIndent(depth)}}`;
 };
 
-const stylish = (data) => {
-  const iter = (tree, depth) => {
-    const result = tree.map((diff) => {
-      switch (diff.type) {
-        case 'added':
-          return `${getStringIndent(depth)}+ ${diff.key}: ${buildString(diff.value, depth)}`;
-        case 'deleted':
-          return `${getStringIndent(depth)}- ${diff.key}: ${buildString(diff.value, depth)}`;
-        case 'changed':
-          return `${getStringIndent(depth)}- ${diff.key}: ${buildString(diff.value1, depth)}\n${getStringIndent(depth)}+ ${diff.key}: ${buildString(diff.value2, depth)}`;
-        case 'unchanged':
-          return `${getStringIndent(depth)}  ${diff.key}: ${buildString(diff.value, depth)}`;
-        case 'nested':
-          return `${getStringIndent(depth)}  ${diff.key}: ${iter(diff.children, depth + 1)}`;
-        default:
-          throw new Error(`Unknown type: ${diff.type}`);
-      }
-    });
+const iter = (nodes, depth) => nodes.map((node) => {
+  switch (node.type) {
+    case 'added':
+      return `${getIndent(depth)}+ ${node.key}: ${stringify(node.value, depth)}`;
+    case 'deleted':
+      return `${getIndent(depth)}- ${node.key}: ${stringify(node.value, depth)}`;
+    case 'changed': {
+      const line1 = `${getIndent(depth)}- ${node.key}: ${stringify(node.value1, depth)}`;
+      const line2 = `${getIndent(depth)}+ ${node.key}: ${stringify(node.value2, depth)}`;
+      return `${line1}\n${line2}`;
+    }
+    case 'unchanged':
+      return `${getIndent(depth)}  ${node.key}: ${stringify(node.value, depth)}`;
+    case 'nested': {
+      const lines = iter(node.children, depth + 1);
+      return `${getIndent(depth)}  ${node.key}: {\n${lines.join('\n')}\n${getIndent(depth)}  }`;
+    }
+    default:
+      throw new Error(`Unknown type: ${node.type}`);
+  }
+});
 
-    return ['{', ...result, `${getBracketIndent(depth)}}`].join('\n');
-  };
+const formatStylish = (tree) => `{\n${iter(tree, 1).join('\n')}\n}`;
 
-  return iter(data, 1);
-};
-
-export default stylish;
+export default formatStylish;
